2438de181057ae3c1c0304ccabe49ea6cdc924b8
okey <bonwuorah@gmail.com>
2018-04-18 14:17:51 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+	 
+
+?>
+
+
+ <!DOCTYPE html>
+<html>
+<head>
+	<meta name='viewport' content='width=device-width,initial-scale=1'>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>HNGI</title>
+</head>
+<body>
+<p>HNGI VOLUME 4.0</p>
+<p>Designed by</p>
+<p>ONWUORAH OKECHUKWU</p>
+<p>Time is : <span><?php
+
+// while(true){
+date("h:i:sa") ." on  ".date("Y/m/d") . "<br>";
+// }
+?></span></p>
+</body>
+</html>
